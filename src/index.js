ad069e3cb52efd56a5bbbff0d61b65dd0dd618d6
rinat_gt <pacmanreal@gmail.com>
2019-07-06 15:57:42 +0500

    Second commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
     var curr = parseInt(currency);
     var res = {};
     if (curr === 0) {
