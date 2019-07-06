77baa8062b7f40b39546ce27cd71d6c44397ffce
rinat_gt <pacmanreal@gmail.com>
2019-07-06 15:23:21 +0500

    My first commit

--- src/index.js
+++ src/index.js
@@ -2,4 +2,27 @@
 module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
+    var curr = parseInt(currency);
+    var res = {};
+    if (curr === 0) {
+        return res = {};
+    }else if (curr > 10000) {
+        return res = {error: "You are rich, my friend! We don't have so much coins for exchange"};
+    }else {
+        var coins = {
+            "H" : 50,
+            "Q" : 25,
+            "D" : 10,
+            "N" : 5,
+            "P" : 1
+        };
+        for (var key in coins) {
+            var quantity = Math.trunc(curr/coins[key]);
+            if (quantity > 0) {
+                res[key] = quantity;
+                curr = curr - coins[key]*res[key];
+            }
+        }
+        return res;
+    }
 }
